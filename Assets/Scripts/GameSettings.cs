b19f43ae22f00d68b02e507c823e98d1d64328ae
Adam Gray <adam.gray@huskers.unl.edu>
2020-02-03 16:55:15 -0600

    Implemented more settings menu functionality.
    Added background music, allowed user to select different options in dropdown, allowed music volume to be changed with slider.

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    public float music_volume;
+    public int skybox_id;
+    public int music_id;
+    public AudioSource music_source;
+}
---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    public Dropdown skybox_selection;
+    public Dropdown music_selection;
+    public Slider music_volume;
+    public AudioSource musicSource;
+
+    public Resolution[] resolutions;
+    private GameSettings game_settings;
+
+    void OnEnable()
+    {
+        game_settings = new GameSettings();
+
+        skybox_selection.onValueChanged.AddListener(delegate { OnSkyboxSelection(); });
+        music_selection.onValueChanged.AddListener(delegate { OnMusicSelection(); });
+        music_volume.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
+        resolutions = Screen.resolutions;
+    }
+
+    public void OnSkyboxSelection()
+    {
+        game_settings.skybox_id = skybox_selection.value;
+    }
+
+    public void OnMusicSelection()
+    {
+        game_settings.music_id = music_selection.value;
+    }
+
+    public void OnMusicVolumeChange()
+    {
+        musicSource.volume = game_settings.music_volume = music_volume.value;
+    }
+
+    public void SaveSettings()
+    {
+
+    }
+
+    public void LoadSettings()
+    {
+
+    }
+}
