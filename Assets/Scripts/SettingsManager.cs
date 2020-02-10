f9ed638237842f0d25f25d32ae996357c04a0c3f
Adam Gray <adam.gray@huskers.unl.edu>
2020-02-10 16:32:51 -0600

    Fixed the bug where a button click would create a bunch of different gameobjects.
    Fixed the bug where a button click would create a bunch of different gameobjects by changing listeners.

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -5,18 +5,17 @@ using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
-    public GameObject[] settings;
-    public GameObject[] mainMenus;
-    public GameObject[] confirmButtons;
-    public GameObject[] settingsButtons;
+    public GameObject settings;
+    public GameObject mainMenu;
+
     public GameObject main_menu_screen;
     public GameObject settings_menu_screen;
     public Dropdown skybox_selection;
     public Dropdown music_selection;
     public Slider music_volume;
     public AudioSource musicSource;
-    public Button confirmButton;
-    public Button settingsButton;
+    public GameObject confirmButton;
+    public GameObject settingsButton;
 
     public Material skyOne;
     public Material skyTwo;
@@ -29,22 +28,19 @@ public class SettingsManager : MonoBehaviour
     public Resolution[] resolutions;
     private GameSettings game_settings;
 
-    void Update()
-    {
-        confirmButtons = GameObject.FindGameObjectsWithTag("ConfirmButton");
-        foreach (GameObject confirmButton in confirmButtons)
-        {
-            Button actualConfirmButton = confirmButton.GetComponent(typeof(Button)) as Button;
-            actualConfirmButton.onClick.AddListener(delegate { onButtonClick(); });
-        }
-        settingsButtons = GameObject.FindGameObjectsWithTag("SettingsButton");
-        foreach (GameObject settingsButton in settingsButtons)
-        {
-            Button actualSettingsButton = settingsButton.GetComponent(typeof(Button)) as Button;
-            actualSettingsButton.onClick.AddListener(delegate { onSettingsButtonClick(); });
-        }
+    private GameObject clone = null;
+    private GameObject clone2 = null;
 
-    }
+    //void Update()
+    //{
+        //confirmButton = GameObject.FindWithTag("ConfirmButton");
+        //Button actualConfirmButton = confirmButton.GetComponent(typeof(Button)) as Button;
+        //actualConfirmButton.onClick.AddListener(delegate { onButtonClick(); });
+        //settingsButton = GameObject.FindWithTag("SettingsButton");
+        //Button actualSettingsButton = settingsButton.GetComponent(typeof(Button)) as Button;
+        //actualSettingsButton.onClick.AddListener(delegate { onSettingsButtonClick(); });
+
+    //}
     void OnEnable()
     {
         game_settings = new GameSettings();
@@ -52,8 +48,8 @@ public class SettingsManager : MonoBehaviour
         skybox_selection.onValueChanged.AddListener(delegate { OnSkyboxSelection(); });
         music_selection.onValueChanged.AddListener(delegate { OnMusicSelection(); });
         music_volume.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
-        confirmButton.onClick.AddListener(delegate { onButtonClick(); });
-        settingsButton.onClick.AddListener(delegate { onSettingsButtonClick(); });
+        //confirmButton.onClick.AddListener(delegate { onButtonClick(); });
+        //settingsButton.onClick.AddListener(delegate { onSettingsButtonClick(); });
 
         resolutions = Screen.resolutions;
     }
@@ -111,42 +107,34 @@ public class SettingsManager : MonoBehaviour
 
     public void onButtonClick()
     {
-        int counter = 0;
-        GameObject clone = null;
-        settings = GameObject.FindGameObjectsWithTag("Settings");
-        foreach (GameObject settings_menu in settings)
-        {
-            if (clone != null)
-            {
-
-            }
-            else
-            {
-                clone = (GameObject) Instantiate(main_menu_screen, settings_menu_screen.transform.position, settings_menu_screen.transform.rotation);
-                game_settings.current_menu = clone;
-                counter++;
-            }
-        Destroy(settings_menu);
+        settings = GameObject.FindWithTag("Settings");
+        GameObject checkMenu = GameObject.FindWithTag("Main Menu");
+        if (checkMenu != null) {
+            Destroy(checkMenu);
+            Instantiate(main_menu_screen, settings_menu_screen.transform.position, settings_menu_screen.transform.rotation);
+            Destroy(settings);
         }
+        else {
+            Instantiate(main_menu_screen, settings_menu_screen.transform.position, settings_menu_screen.transform.rotation);
+        }
+        //Instantiate(settings_menu_screen, main_menu_screen.transform.position, main_menu_screen.transform.rotation);
+        Destroy(settings);
     }
 
     public void onSettingsButtonClick()
     {
-        int counter2 = 0;
-        GameObject clone = null;
-        mainMenus = GameObject.FindGameObjectsWithTag("Main Menu");
-        foreach (GameObject main_menu in mainMenus)
+        mainMenu = GameObject.FindWithTag("Main Menu");
+        GameObject checkSettings = GameObject.FindWithTag("Settings");
+        if (checkSettings != null)
         {
-            if (clone != null)
-            {
-
-            } else
-            {
-                clone = (GameObject) Instantiate(settings_menu_screen, main_menu_screen.transform.position, main_menu_screen.transform.rotation);
-                game_settings.current_menu = clone;
-                counter2++;
-            }
-        Destroy(main_menu);
+            Destroy(checkSettings);
+            Instantiate(settings_menu_screen, main_menu_screen.transform.position, main_menu_screen.transform.rotation);
+            Destroy(mainMenu);
+        } else {
+            Instantiate(settings_menu_screen, main_menu_screen.transform.position, main_menu_screen.transform.rotation);
         }
+        //Instantiate(settings_menu_screen, main_menu_screen.transform.position, main_menu_screen.transform.rotation);
+        Destroy(mainMenu);
+
     }
 }
